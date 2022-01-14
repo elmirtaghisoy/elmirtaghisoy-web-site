f44c90ebe6b66850aa142d5a2b3a4b0ad432146e
TagiyevEZ <elmir.tagiyev@kapitalbank.az>
2022-01-14 20:40:00 +0400

    Initial commit

---
+++ src/main/java/az/elmirtaghisoywebsite/ElmirtaghisoyWebSiteApplication.java
@@ -0,0 +1,13 @@
+package az.elmirtaghisoywebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElmirtaghisoyWebSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElmirtaghisoyWebSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/az/elmirtaghisoywebsite/ElmirtaghisoyWebSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package az.elmirtaghisoywebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElmirtaghisoyWebSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
